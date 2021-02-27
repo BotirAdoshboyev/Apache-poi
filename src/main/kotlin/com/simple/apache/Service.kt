479d5e9656023aa262ad8953f4edc21af4758846
Botir Adoshboyev <67525758+BotirAdoshboyev@users.noreply.github.com>
2021-02-28 02:25:57 +0500

    initial commit

--- src/main/kotlin/com/simple/apache/Service.kt
+++ src/main/kotlin/com/simple/apache/Service.kt
@@ -1,6 +1,6 @@
 package com.simple.apache
 
-import org.apache.poi.ss.util.CellUtil.createCell
+import org.apache.poi.ss.usermodel.DateUtil
 import org.apache.poi.xssf.usermodel.XSSFCellStyle
 import org.apache.poi.xssf.usermodel.XSSFFont
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
@@ -8,26 +8,22 @@ import org.springframework.http.*
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import java.io.ByteArrayOutputStream
-import java.io.OutputStream
-import java.util.*
+import java.text.SimpleDateFormat
+
+
 
 data class Dto(
     var id: Long,
     var name: String,
     var total: Long,
-    var new: Long,
-    var ready: Long,
-    var issue: Long,
-    var print: Long
+    var type: Long,
+    var date: Long
 )
 
-
-fun getItems(): List<Dto>{
-
+fun getItems(date: Long, type: Long): List<Dto>{
     val res = mutableListOf<Dto>()
-
-    for (i in 0..30){
-        val item = Dto(1, "Bdlewkmdkwednwjndotir", 250, 4000, 253, 500, 265)
+    for (i in 0..10){
+        val item = Dto(i.toLong(), "Samarqand $i", 20+i.toLong(), type, date)
         res.add(i, item)
     }
     return res
@@ -36,97 +32,80 @@ fun getItems(): List<Dto>{
 class Controller {
     @PostMapping
     fun post() : ResponseEntity<ByteArray>{
-        var rowIndex = 1
-        val list = getItems()
-
+        var totalInRegions = mutableMapOf<Long, Long>()
+        var currentType = 0
+        val typeList = listOf(501, 502, 503)
+        var startDate = 1608527600000
+        val endDate = 1609927600000
+        val dateList = mutableListOf<Long>()
+        while (startDate <= endDate) {
+            dateList.add(startDate)
+            startDate += DateUtil.DAY_MILLISECONDS
+        }
         val xlWB = XSSFWorkbook()
-        val xlWS = xlWB.createSheet("statistika")
-
         val boldCellStyle: XSSFCellStyle = xlWB.createCellStyle()
         val isBold: XSSFFont = xlWB.createFont()
-
         isBold.bold = true
         boldCellStyle.setFont(isBold)
+        val pattern = SimpleDateFormat("dd-MM-yyyy")
+        typeList.forEach { type ->
+            if (currentType != type) {
+                totalInRegions = mutableMapOf()
+            }
+            val xlWS = when(type) {
+                501 -> xlWB.createSheet("Fuqoro")
+                502 -> xlWB.createSheet("IR")
+                else -> xlWB.createSheet("LBG")
+            }
+            var totalInDate = 0
+            for (i in dateList.indices) {
+                val list = getItems(dateList[i], type.toLong())
+                list.forEach {
+                    if (totalInRegions[it.id] == null) {
+                        totalInRegions[it.id] = it.total
+                    } else {
+                        val oldVal = totalInRegions[it.id]
+                        totalInRegions[it.id] = it.total + oldVal!!
+                    }
+                }
+                if (i == 0) {
+                    for (j in 0..list.size+5) {
+                        xlWS.createRow(j)
+                    }
+                }
+                xlWS.createFreezePane(0, 1)
+                list.forEachIndexed { index, dto ->
+                    if (index == 0) {
+                        val headerNameCell = xlWS.getRow(0).createCell(0)
+                        headerNameCell.setCellValue("Xudud")
+                        xlWS.autoSizeColumn(0)
+                        headerNameCell.cellStyle = boldCellStyle
+                        val headerDateCell = xlWS.getRow(index).createCell(i+1)
+                        headerDateCell.setCellValue(pattern.format(dateList[i]))
+                        headerDateCell.cellStyle = boldCellStyle
+                    }
+                    xlWS.getRow(index+1).createCell(0).setCellValue(dto.name)
+                    xlWS.getRow(index+1).createCell(i+1).setCellValue(list[index].total.toString())
+                    xlWS.autoSizeColumn(i+1)
+                    totalInDate += list[index].total.toInt()
+                }
+                val footerTotalCell = xlWS.getRow(list.lastIndex+2).createCell(i+1)
+                footerTotalCell.setCellValue(totalInDate.toString())
+                footerTotalCell.cellStyle = boldCellStyle
+                totalInDate = 0
+                val headerTotalCell = xlWS.getRow(0).createCell(dateList.lastIndex+2)
+                headerTotalCell.setCellValue("Jami")
+                headerTotalCell.cellStyle = boldCellStyle
 
-        val headerRow = xlWS.createRow(0)
-
-        val headerIdCell = createCell(headerRow, 0, "Id")
-        headerIdCell.cellStyle = boldCellStyle
-        val headerNameCell = createCell(headerRow, 1, "Viloyat")
-        headerNameCell.cellStyle = boldCellStyle
-        val headerTotalCell = createCell(headerRow, 2, "Umumiy")
-        headerTotalCell.cellStyle = boldCellStyle
-        val headerNewCell = createCell(headerRow,3, "Yangi")
-        headerNewCell.cellStyle = boldCellStyle
-        val headerReadyCell = createCell(headerRow, 4, "Tayyor")
-        headerReadyCell.cellStyle = boldCellStyle
-        val headerIssueCell = createCell(headerRow, 5, "Berilgan")
-        headerIssueCell.cellStyle = boldCellStyle
-        val headerPrintCell = createCell(headerRow, 6, "Pechat qilingan")
-        headerPrintCell.cellStyle = boldCellStyle
-
-        var totalOfTotal: Long = 0
-        var totalOfNew: Long = 0
-        var totalOfReady: Long = 0
-        var totalOfIssue: Long = 0
-        var totalOfPrint: Long = 0
-
-        list.forEach{
-            totalOfTotal += it.total
-            totalOfNew += it.new
-            totalOfReady += it.ready
-            totalOfIssue += it.issue
-            totalOfPrint += it.print
-
-            val xlRow = xlWS.createRow(rowIndex++)
-
-            val idCell = xlRow.createCell(0)
-            idCell.setCellValue(it.id.toString())
-
-            val nameCell = xlRow.createCell(1)
-            nameCell.setCellValue(it.name)
-
-            val totalCell = xlRow.createCell(2)
-            totalCell.setCellValue(it.total.toString())
-
-            val newCell = xlRow.createCell(3)
-            newCell.setCellValue(it.new.toString())
-
-            val readyCell = xlRow.createCell(4)
-            readyCell.setCellValue(it.ready.toString())
-
-            val issueCell = xlRow.createCell(5)
-            issueCell.setCellValue(it.issue.toString())
-
-            val printCell = xlRow.createCell(6)
-            printCell.setCellValue(it.print.toString())
+                val footerTotalNamedCell = xlWS.getRow(list.lastIndex+2).createCell(0)
+                footerTotalNamedCell.setCellValue("Jami")
+                footerTotalNamedCell.cellStyle = boldCellStyle
+                currentType = type
+                for ((k, v) in totalInRegions.values.toList().withIndex()) {
+                    xlWS.getRow(k+1).createCell(dateList.lastIndex+2).setCellValue(v.toString())
+                }
+            }
         }
-        val footerRow = xlWS.createRow(rowIndex)
-        val footerTextCell = createCell(footerRow, 1, "Umumiy")
-        footerTextCell.cellStyle = boldCellStyle
-        val footerTotalCell = createCell(footerRow, 2, totalOfTotal.toString())
-        footerTotalCell.cellStyle = boldCellStyle
-        val footerNewCell = createCell(footerRow, 3, totalOfNew.toString())
-        footerNewCell.cellStyle = boldCellStyle
-        val footerReadyCell = createCell(footerRow, 4, totalOfReady.toString())
-        footerReadyCell.cellStyle = boldCellStyle
-        val footerIssueCell = createCell(footerRow, 5, totalOfIssue.toString())
-        footerIssueCell.cellStyle = boldCellStyle
-        val footerPrintCell = createCell(footerRow, 6, totalOfPrint.toString())
-        footerPrintCell.cellStyle = boldCellStyle
-
-//        for (i in 0..6){
-//            xlWS.autoSizeColumn(i)
-//        }
-
-        xlWS.autoSizeColumn(0)
-        xlWS.autoSizeColumn(1)
-        xlWS.autoSizeColumn(2)
-        xlWS.autoSizeColumn(3)
-        xlWS.autoSizeColumn(4)
-        xlWS.autoSizeColumn(5)
-        xlWS.autoSizeColumn(6)
-        xlWS.createFreezePane(0, 1)
         val byteArrayOS = ByteArrayOutputStream()
         xlWB.write(byteArrayOS)
         xlWB.close()
