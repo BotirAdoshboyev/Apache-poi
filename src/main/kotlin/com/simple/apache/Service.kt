139a57d46598106489cd556721c07e29af032d49
Botir Adoshboyev <67525758+BotirAdoshboyev@users.noreply.github.com>
2021-03-03 19:35:57 +0500

    initial commit

--- src/main/kotlin/com/simple/apache/Service.kt
+++ src/main/kotlin/com/simple/apache/Service.kt
@@ -6,10 +6,16 @@ import org.apache.poi.xssf.usermodel.XSSFFont
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.springframework.http.*
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
-
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.util.*
 
 
 data class Dto(
@@ -20,28 +26,32 @@ data class Dto(
     var date: Long
 )
 
+data class IntervalDto(
+    val startDate: Long,
+    val endDate: Long
+)
+
 fun getItems(date: Long, type: Long): List<Dto>{
     val res = mutableListOf<Dto>()
+    var k = 0
     for (i in 0..10){
-        val item = Dto(i.toLong(), "Samarqand $i", 20+i.toLong(), type, date)
+        k += 4
+        val item = Dto(i.toLong(), "Samarqand $i", 20+i.toLong() + k.toLong(), type, date)
         res.add(i, item)
     }
     return res
 }
 @RestController
+@RequestMapping
 class Controller {
     @PostMapping
     fun post() : ResponseEntity<ByteArray>{
         var totalInRegions = mutableMapOf<Long, Long>()
         var currentType = 0
         val typeList = listOf(501, 502, 503)
-        var startDate = 1608527600000
-        val endDate = 1609927600000
-        val dateList = mutableListOf<Long>()
-        while (startDate <= endDate) {
-            dateList.add(startDate)
-            startDate += DateUtil.DAY_MILLISECONDS
-        }
+        val startDate = 1614097999402
+        val endDate = 1614761199000
+        val dateList = makeDateInterval(IntervalDto(startDate, endDate))
         val xlWB = XSSFWorkbook()
         val boldCellStyle: XSSFCellStyle = xlWB.createCellStyle()
         val isBold: XSSFFont = xlWB.createFont()
@@ -54,12 +64,24 @@ class Controller {
             }
             val xlWS = when(type) {
                 501 -> xlWB.createSheet("Fuqoro")
-                502 -> xlWB.createSheet("IR")
+                503 -> xlWB.createSheet("IR")
                 else -> xlWB.createSheet("LBG")
             }
             var totalInDate = 0
-            for (i in dateList.indices) {
-                val list = getItems(dateList[i], type.toLong())
+            dateList.forEachIndexed { i, date ->
+                var endedDate: Long? = null
+                endedDate = if (dateList.size > 1 && dateList.lastIndex != i) {
+                    dateList[i] + DateUtil.DAY_MILLISECONDS - 1
+                } else {
+                    endDate
+                }
+                val startedDate = if (i == 0) {
+                    startDate
+                } else {
+                    dateList[i]
+                }
+                println("$startedDate, $endedDate")
+                val list = getItems(date, type.toLong())
                 list.forEach {
                     if (totalInRegions[it.id] == null) {
                         totalInRegions[it.id] = it.total
@@ -81,13 +103,13 @@ class Controller {
                         xlWS.autoSizeColumn(0)
                         headerNameCell.cellStyle = boldCellStyle
                         val headerDateCell = xlWS.getRow(index).createCell(i+1)
-                        headerDateCell.setCellValue(pattern.format(dateList[i]))
+                        headerDateCell.setCellValue(pattern.format(date))
                         headerDateCell.cellStyle = boldCellStyle
                     }
                     xlWS.getRow(index+1).createCell(0).setCellValue(dto.name)
-                    xlWS.getRow(index+1).createCell(i+1).setCellValue(list[index].total.toString())
+                    xlWS.getRow(index+1).createCell(i+1).setCellValue(dto.total.toString())
                     xlWS.autoSizeColumn(i+1)
-                    totalInDate += list[index].total.toInt()
+                    totalInDate += dto.total.toInt()
                 }
                 val footerTotalCell = xlWS.getRow(list.lastIndex+2).createCell(i+1)
                 footerTotalCell.setCellValue(totalInDate.toString())
@@ -113,10 +135,23 @@ class Controller {
 
         val headers = HttpHeaders().apply {
             contentType = MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8")
-            contentDisposition = ContentDisposition.builder("inline").filename("test.xlsx").build()
+            contentDisposition = ContentDisposition.builder("inline").filename("${LocalDate.now()}.xlsx").build()
             contentLength = byteArrayOS.size().toLong()
         }
 
         return ResponseEntity(byteArrayOS.toByteArray(), headers, HttpStatus.OK)
     }
+}
+
+fun makeDateInterval(dto: IntervalDto): List<Long> {
+    var startDate = Instant.ofEpochMilli(dto.startDate).atZone(ZoneId.systemDefault()).toLocalDate().atStartOfDay().toEpochSecond(
+        ZoneOffset.UTC).times(1000)
+    val endDate = Instant.ofEpochMilli(dto.endDate).atZone(ZoneId.systemDefault()).toLocalDate().atStartOfDay().toEpochSecond(
+        ZoneOffset.UTC).times(1000)
+    val dateList = mutableListOf<Long>()
+    while (startDate <= endDate) {
+        dateList.add(startDate)
+        startDate += DateUtil.DAY_MILLISECONDS
+    }
+    return dateList
 }
